61110f11854714ec970b86dcd3301157d4fa0e81
ernesteNtezirizaza <erneste.ntezirizaza@gmail.com>
2025-09-08 15:48:16 +0200

    done

--- supervised_learning/classification/21-deep_neural_network.py
+++ supervised_learning/classification/21-deep_neural_network.py
@@ -139,4 +139,3 @@ class DeepNeuralNetwork:
             da = np.matmul(W.T, dz)
             self.__weights['W' + str(i)] -= (alpha * dw)
             self.__weights['b' + str(i)] -= (alpha * db)
-            
